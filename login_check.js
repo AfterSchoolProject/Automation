074711b6db6df7321cd8dce819fbd49236ebefa9
addriv <adrivero89@gmail.com>
2017-11-01 21:53:20 -0700

    logged in

---
+++ login_check.js
@@ -0,0 +1,21 @@
+const Nightmare = require('nightmare');
+
+const nightmare = Nightmare({show: true});
+
+const login_check = () => {
+  console.log('Attempting login...');
+
+  nightmare.goto('https://angel.co/login')
+    .wait(2000)
+    .type('input#user_email', 'adrivero89@gmail.com')
+    .type('input#user_password', 'A@dRian89')
+    .click('input[value="Log In"]')
+    .wait(2000)
+    .click('li.hide_when_handheld.u-inlineBlock.u-vAlignTop > a')
+    .wait(10000)
+    // .evaluate(() => document.querySelectorAll('.u-uncoloredLink')[0].text)
+    .end()
+    .then(result => console.log(`Finished ${result}`));
+};
+
+login_check();
