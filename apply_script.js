5d79d63e1d441371362136187ea9fed39a237b46
addriv <adrivero89@gmail.com>
2017-12-11 15:56:58 -0800

    Update script

--- apply_script.js
+++ apply_script.js
@@ -7,17 +7,18 @@ $('.startup-title').text().replace('Apply to ', '');
 // Text area value
 $('.container > textarea').val();
 
-const coverLetter = () => {
+coverLetter = () => {
   const recruiter = $('span.name').text().split(' ')[0];
   const company = $('.startup-title').text().replace('Apply to ', '');
   const text = (
-    `Hi ${recruiter},\n\nI saw your post and believe my skills in JavaScript, React, Redux, Ruby on Rails, Node.js, closely match the requirements for the position.\n\nMy background is in Aerospace Engineering and for the last 4 years, I've worked as a Process Engineer for a semiconductor company where my work ethic and passion for engineering enabled me to successfully spearhead projects to reduce costs and improve yield; increasing revenue by $4 + million annually. I decided to pursue a career in software and attended App Academy, a top coding school in San Francisco with less than 3% acceptance rate where I mastered web development technologies.\n\nI know you must be busy but I was hoping I could get 10 minutes of your time for a quick phone conversation.I would love to learn more about ${company} and understand what you’re looking for in a candidate.\n\nWould you be free sometime next week? My resume can be found on www.adrianrivero.me for your reference. I look forward to connecting!\n\nBest,\nAdrian`
+    `Hi ${recruiter},\n\nI saw your post and believe my skills in JavaScript, React, Redux, Ruby on Rails, Node.js closely match the requirements for ${company}.\n\nMy background is in Aerospace Engineering and for the last 4 years, I've worked as a Process Engineer for a semiconductor company where my work ethic and passion for engineering enabled me to successfully spearhead projects to reduce costs and improve yield; increasing revenue by $4 + million annually. I decided to pursue a career in software and attended App Academy, a top coding school in San Francisco with less than 3% acceptance rate where I mastered web development technologies.\n\nI know you must be busy but I was hoping I could get 10 minutes of your time for a quick phone conversation. I would love to learn more about ${company} and understand what you’re looking for in a candidate.\n\nWould you be free sometime next week? My resume can be found on www.adrianrivero.me for your reference. I look forward to connecting!\n\nBest,\nAdrian`
   );
 
+  // Change textarea value to cover letter
   $('.container > textarea').val(text);
 };
 
-
+coverLetter();
 
 
 // Hi xxxxxx,
