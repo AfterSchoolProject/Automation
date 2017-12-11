1bad95dee2ed13fc9783a6e6388ffe4fe5551a25
addriv <adrivero89@gmail.com>
2017-12-11 14:00:11 -0800

    Add cover letter

--- apply_script.js
+++ apply_script.js
@@ -1,6 +1,32 @@
+import coverLetter from './apply.js';
 // Job title
 $('.job-title > a').text();
 // Poster name
 $('span.name').text();
 // Company name
 $('.startup-title').text().replace('Apply to ', '');
+// Text area value
+$('.container > textarea').val();
+
+const coverLetter = (recruiter, position, company) => {
+  return (
+    `Hello Arash,\nMy name is Adrian and I am a software engineer who enjoys building solutions to everyday problems through the power of code.\nI believe have the ability to quickly adapt to ShareGrid's stack with my experience in various technologies including Ruby on Rails, React, React Native, Redux, and JavaScript.\nI'd love to connect with you to further discuss the Ruby on Rails Software Engineer position.\nBest Regards,\nAdrian Rivero`
+  );
+};
+
+Hi xxxxxx,
+
+I saw the job post and believe my skills closely match the requirements
+for the position. My most recent projects include web applications built with JavaScript, React, React Native, Redux, Ruby on Rails, Node.js, etc.
+
+A little about me, I’ m a software engineer who enjoys building solutions to everyday problems through the power of code.My background is in Aerospace Engineering and since college(last 4 years), I have worked as a Process Engineer
+for a semiconductor company where my work ethic and passion
+for engineering enabled me to successfully spearhead several projects to reduce costs and improve yield;
+increasing revenue by $4 + million annually.
+
+I know you must be super busy but I was hoping I could get 10 minutes of your time for a quick phone conversation.I would love to learn more about Riot Games and understand what you’re looking for in a candidate.
+
+Would you be free sometime next week ? For your reference, my resume can be found on www.adrianrivero.me. I look forward to connecting!
+
+Thank you
+for your time.I look forward to speaking with you.
